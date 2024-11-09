1041d37ed9cdc6782b256150e4fa84fe6695142e
蒲公英 <77421578+YANXIAOXIH@users.noreply.github.com>
2024-11-09 18:07:38 +0800

    Add files via upload

--- include/dt-bindings/pinctrl/rockchip.h
+++ include/dt-bindings/pinctrl/rockchip.h
@@ -1,13 +1,29 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
 /*
  * Header providing constants for Rockchip pinctrl bindings.
  *
  * Copyright (c) 2013 MundoReader S.L.
  * Author: Heiko Stuebner <heiko@sntech.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
 
 #ifndef __DT_BINDINGS_ROCKCHIP_PINCTRL_H__
 #define __DT_BINDINGS_ROCKCHIP_PINCTRL_H__
+
+#define RK_GPIO0	0
+#define RK_GPIO1	1
+#define RK_GPIO2	2
+#define RK_GPIO3	3
+#define RK_GPIO4	4
+#define RK_GPIO6	6
 
 #define RK_PA0		0
 #define RK_PA1		1
@@ -43,5 +59,21 @@
 #define RK_PD7		31
 
 #define RK_FUNC_GPIO	0
+#define RK_FUNC_0	0
+#define RK_FUNC_1	1
+#define RK_FUNC_2	2
+#define RK_FUNC_3	3
+#define RK_FUNC_4	4
+#define RK_FUNC_5	5
+#define RK_FUNC_6	6
+#define RK_FUNC_7	7
+#define RK_FUNC_8	8
+#define RK_FUNC_9	9
+#define RK_FUNC_10	10
+#define RK_FUNC_11	11
+#define RK_FUNC_12	12
+#define RK_FUNC_13	13
+#define RK_FUNC_14	14
+#define RK_FUNC_15	15
 
 #endif
